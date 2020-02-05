a5e6bd0e2abfeea6a16a27ca33397caf72f32014
adityachirania <52464668+adityachirania@users.noreply.github.com>
2020-02-05 14:33:01 +0530

    Adding tables needed for now

--- jobfinder/admin.py
+++ jobfinder/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Profile
+from .models import Category
+from .models import SkillSet
+from .models import Job
+from .models import JobCategory
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Category)
+admin.site.register(SkillSet)
+admin.site.register(JobCategory)
+admin.site.register(Job)
--- jobfinder/models.py
+++ jobfinder/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    Username = models.OneToOneField(User, on_delete=models.CASCADE)
+    FirstName = models.CharField(max_length=50)
+    LastName = models.CharField(max_length=50)
+    PhoneNumber = models.CharField( max_length=10)
+    Address = models.CharField(max_length=200)
+
+class Category(models.Model):
+    Tag = models.CharField(max_length=30)
+    Characteristics = models.ManyToManyField(
+        Profile, through="SkillSet", through_fields=("CategoryTags","Profile"),
+    )
+
+class SkillSet(models.Model):
+    Profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    CategoryTags = models.ForeignKey(Category,on_delete=models.CASCADE)
+    
+
+class Job(models.Model):
+    TYPE_CHOICES=[
+        ("Skilled","Skilled"),
+        ("Unskilled","Unskilled")
+        ]
+    Poster = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    Type = models.CharField(choices=TYPE_CHOICES, max_length=10)
+    Descripton = models.CharField(max_length=500)
+    Assigned = models.BooleanField()
+    ApplyBy = models.DateField()
+    Characteristics = models.ManyToManyField(
+        Category, through="JobCategory", through_fields=("Job", "Category"),
+    )
+
+class JobCategory(models.Model):
+    Job = models.ForeignKey(Job,on_delete=models.CASCADE)
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+

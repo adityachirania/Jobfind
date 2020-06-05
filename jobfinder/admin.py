ff4a699be55b0aec26460d31b7ad1d88ed848bfe
adityachirania <52464668+adityachirania@users.noreply.github.com>
2020-06-05 20:11:34 +0530

    added some changes

--- jobfinder/admin.py
+++ jobfinder/admin.py
@@ -6,12 +6,54 @@ from .models import Job
 from .models import JobCategory
 from .models import JobConnection
 
+
+admin.site.site_header = 'Jobfinder'
+
+class profileAdmin(admin.ModelAdmin):
+    list_display = ["Username", "FirstName", "LastName", "Address"]
+
+class categoryAdmin(admin.ModelAdmin):
+    list_display = ["id","Tag"]
+
+class skillsetAdmin(admin.ModelAdmin):
+    list_display = ["user", "CategoryTags"]
+
+    def user(self,obj):
+        return obj.Profile.Username
+
+class jobAdmin(admin.ModelAdmin):
+    list_display = ["poster","Descripton","Assigned","ApplyBy"]
+
+    def poster(self,obj):
+        return obj.Poster.Username
+
+class jobcategoryAdmin(admin.ModelAdmin):
+    list_display = ["title", "tag"]
+
+    def title(self,obj):
+        return obj.Job.Title
+    def tag(self,obj):
+        return obj.Category.Tag
+
+class jobconnectionAdmin(admin.ModelAdmin):
+    list_display = ["title","poster","employee","Completed"]
+
+    def poster(self,obj):
+        return obj.Jobid.Poster.Username
+    def title(self,obj):
+        return obj.Jobid.Title
+    def employee(self,obj):
+        return obj.Employee.Username
+    
 # Register your models here.
-admin.site.register(Profile)
-admin.site.register(Category)
-admin.site.register(SkillSet)
-admin.site.register(JobCategory)
-admin.site.register(Job)
-admin.site.register(JobConnection)
+admin.site.register(Profile,profileAdmin)
+admin.site.register(Category,categoryAdmin)
+admin.site.register(SkillSet,skillsetAdmin)
+admin.site.register(JobCategory,jobcategoryAdmin)
+admin.site.register(Job,jobAdmin)
+admin.site.register(JobConnection,jobconnectionAdmin)
+
+    
+
 
 
--- jobfinder/models.py
+++ jobfinder/models.py
@@ -9,15 +9,39 @@ class Profile(models.Model):
     PhoneNumber = models.CharField( max_length=10)
     Address = models.CharField(max_length=200)
 
+    class Meta:
+        """Declare the metadata of the model"""
+        verbose_name_plural = 'Profiles'
+        verbose_name = 'Profile'
+
+    def __str__(self):
+        """Convert entry of class Employee to a string representation."""
+        return "{}".format(self.Username)
+
+
 class Category(models.Model):
     Tag = models.CharField(max_length=30)
     Characteristics = models.ManyToManyField(
         Profile, through="SkillSet", through_fields=("CategoryTags","Profile"),
     )
 
+    class Meta:
+        """Declare the metadata of the model"""
+        verbose_name_plural = 'Categories'
+        verbose_name = 'Category'
+
+    def __str__(self):
+        """Convert entry of class Employee to a string representation."""
+        return "{}".format(self.Tag)
+
 class SkillSet(models.Model):
     Profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
     CategoryTags = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+    class Meta:
+        """Declare the metadata of the model"""
+        verbose_name_plural = 'Skill Sets'
+        verbose_name = 'Skill Set'
     
 
 class Job(models.Model):
@@ -26,6 +50,7 @@ class Job(models.Model):
         ("Unskilled","Unskilled")
         ]
     Poster = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    Title = models.CharField(max_length=50)
     Type = models.CharField(choices=TYPE_CHOICES, max_length=10)
     Descripton = models.CharField(max_length=500)
     Assigned = models.BooleanField()
@@ -34,15 +59,36 @@ class Job(models.Model):
         Category, through="JobCategory", through_fields=("Job", "Category"),
     )
 
+    class Meta:
+        """Declare the metadata of the model"""
+        verbose_name_plural = 'Jobs'
+        verbose_name = 'Job'
+
+    def __str__(self):
+        """Convert entry of class Employee to a string representation."""
+        return "{}".format(self.Title)
+
 class JobCategory(models.Model):
     Job = models.ForeignKey(Job,on_delete=models.CASCADE)
     Category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
+    class Meta:
+        """Declare the metadata of the model"""
+        verbose_name_plural = 'Job-Category connections'
+        verbose_name = 'Job-category connection'
+
+    def __str__(self):
+        """Convert entry of class Employee to a string representation."""
+        return "{}-{}".format(self.Job.Title,self.Category.Tag)
+
 class JobConnection(models.Model):
     Jobid =    models.ForeignKey(Job,on_delete=models.CASCADE)
-    Employer = models.ForeignKey(Profile,on_delete=models.CASCADE,related_name='employer')
     Employee = models.ForeignKey(Profile,on_delete=models.CASCADE,related_name='employee')
     Completed = models.BooleanField()
 
+    class Meta:
+        """Declare the metadata of the model"""
+        verbose_name_plural = 'Job Connections'
+        verbose_name = 'Job Connection'
 
 
